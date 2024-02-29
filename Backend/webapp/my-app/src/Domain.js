7f517abda231c2079808e51cfa5dec542039cc5f
khlaun1 <60130354+khlaun1@users.noreply.github.com>
2024-02-29 11:08:55 -0500

    removed all files

--- Backend/webapp/my-app/src/Domain.js
+++
@@ -1,9 +0,0 @@
-const acceptableDomains = {
-    "gmail.com":true,
-    "yahoo.com":true,
-    "hotmail.com":true,
-    "outlook.com":true,
-    "odu.edu":true
-};  
-
-export default acceptableDomains;
